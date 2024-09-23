38909f3d120e914e0608556efc2215b59d7e3bde
Arturo Garcia <arturogfin@gmail.com>
2024-09-23 10:06:31 -0300

    [ADD] funcoes finalizadas - erro pop_back

--- deque.c
+++ deque.c
@@ -82,7 +82,104 @@ int pop_front (struct deque *d)
 
 void push_back (struct deque *d, int dado)
 {
-    struct nodo *new, *aux;
+    struct nodo *new;
+
+    new = malloc(sizeof(struct nodo));
+    if (new == NULL)
+    {
+        printf ("Falha na alocacao\n");
+        return;
+    }
+
+    new->data = dado;
+    new->next = NULL;
+    new->prev = NULL;
+
+    if (deque_empty(d))
+    {
+        d->str = new;
+        d->end = new;
+        (d->size)++;
+
+        return;
+    }
+
+    if (deque_size(d) == 1)
+    {
+        new->prev = d->str;
+        d->end = new;
+        d->str->next = new;
+        (d->size)++;
+
+        return;
+    }
+
+    d->end->next = new;
+    new->prev = d->end;
+    d->end = new;
+    (d->size)++;
+
+    return;
+}
+
+int pop_back (struct deque *d)
+{
+    struct nodo *aux;
+    int dado;
+
+    if (deque_empty(d))
+    {
+        printf ("Falha: deque vazio\n");
+        return 0;
+    }
+
+    aux = d->end;
+    dado = aux->data;
+    d->end = d->end->prev;
+    (d->size)--;
+
+    free (aux);
+    aux = NULL;
+
+    return dado;
+}
+
+int peek_front (struct deque *d)
+{
+    if (deque_empty(d))
+    {
+        printf ("Falha: deque vazio");
+        return 0;
+    }
+
+    return d->str->data;
+}
+
+int peek_back (struct deque *d)
+{
+    if (deque_empty(d))
+    {
+        printf ("Falha: deque vazio");
+        return 0;
+    }
+    
+    return d->end->data;
+}
+
+void deque_destroy (struct deque **d)
+{
+    struct nodo *aux;
+
+    while ((*d)->str != NULL)
+    {
+        aux = (*d)->str;
+        (*d)->str = (*d)->str->next;
+        free(aux);
+        aux = NULL;
+    }
+
+    free (*d);
+    *d = NULL;
 }
 
 char deque_empty (struct deque *d)
